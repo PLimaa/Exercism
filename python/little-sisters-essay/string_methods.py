ddb8fce08c9cd00249f9b6c218407fccb38037ed
PLimaa <pedro.lima.20231@poli.ufrj.br>
2026-04-05 21:41:32 -0300

    solucao exercicio string methods

--- python/little-sisters-essay/string_methods.py
+++ python/little-sisters-essay/string_methods.py
@@ -7,9 +7,7 @@ def capitalize_title(title):
     :param title: str - title string that needs title casing.
     :return: str - title string in title case (first letters capitalized).
     """
-
-    pass
-
+    return str.title(title)
 
 def check_sentence_ending(sentence):
     """Check the ending of the sentence to verify that a period is present.
@@ -17,8 +15,7 @@ def check_sentence_ending(sentence):
     :param sentence: str - a sentence to check.
     :return: bool - return True if punctuated correctly with period, False otherwise.
     """
-
-    pass
+    return sentence.endswith('.')
 
 
 def clean_up_spacing(sentence):
@@ -27,8 +24,7 @@ def clean_up_spacing(sentence):
     :param sentence: str - a sentence to clean of leading and trailing space characters.
     :return: str - a sentence that has been cleaned of leading and trailing space characters.
     """
-
-    pass
+    return sentence.strip()
 
 
 def replace_word_choice(sentence, old_word, new_word):
@@ -40,4 +36,7 @@ def replace_word_choice(sentence, old_word, new_word):
     :return: str - input sentence with new words in place of old words.
     """
 
-    pass
+    return sentence.replace(old_word, new_word)
+
+
+
