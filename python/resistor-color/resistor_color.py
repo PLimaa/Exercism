932c4186ddfa29bde264e22844e307cbfd3168d7
PLimaa <pedro.lima.20231@poli.ufrj.br>
2026-04-20 17:20:16 -0300

    adicao exercicio e solucao resistor color code

---
+++ python/resistor-color/resistor_color.py
@@ -0,0 +1,20 @@
+def color_code(color):
+    cores = colors()
+    valor = cores.index(color)
+    return valor
+
+
+
+def colors():
+    return [
+        "black", 
+        "brown",
+        "red",
+        "orange",
+        "yellow",
+        "green",
+        "blue",
+        "violet",
+        "grey",
+        "white"
+        ]
