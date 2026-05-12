03e0f4b325a214d8f3db4541eab03f44907e8dc6
Pedro Lima <pedro.lima.20231@poli.ufrj.br>
2026-05-11 22:25:25 -0300

    atualizacao solucao hamming distance
    melhorias em questao de boas praticas e clean code

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,10 +1,10 @@
 def distance(strand_a, strand_b):
-    distance = 0
+    count = 0
     if len(strand_a) == len(strand_b):
         for i in range(len(strand_a)):
             if strand_a[i] != strand_b[i]:
-                distance += 1
-        return distance
-    else: raise ValueError("Strands must be of equal length.")
+                count += 1
+        return count
+    raise ValueError("Strands must be of equal length.")
 
 
